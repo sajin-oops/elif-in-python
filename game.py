d13fce7e608e4d89eec9ad07a84bd823765b99ef
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-10 19:17:22 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,11 @@
+grade = int(input("Enter your score: "))
+if grade >= 90:
+    print("You got an A!")
+elif grade >= 80:
+    print("You got a B!")
+elif grade >= 70:
+    print("You got a C!")
+elif grade >= 60:
+    print("You got a D!")
+else:
+    print("You got an F.")
